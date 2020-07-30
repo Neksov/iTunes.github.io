b9246f7a7908aac7cee92e7db1d7eadadb86ab48
Neksov <n2v@list.ru>
2020-07-30 11:40:28 +0300

    new block audio

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,3 +1,53 @@
 export const radioPlayerInit = () => {
-  console.log("Radio Init");
-}
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+  const audio = new Audio();
+
+  audio.type = 'audio/aac';
+  radioStop.disabled = true; //блокируем кнопку плей
+
+  const changeIconPlay = () => { //меняем иконку плей на стоп 
+    if (audio.paused) {
+      radio.classList.remove('play'); //анимация динамика 
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radio.classList.add('play'); //анимация динамика 
+      radioStop.classList.add('fa-stop');
+      radioStop.classList.remove('fa-play');
+
+    }
+  }
+  const selectItem = elem => {
+    radioItem.forEach(item => item.classList.remove('select')); //убираем серый ободок после выбора следующей станции
+    elem.classList.add('select');
+  };
+  radioNavigation.addEventListener('change', event => {
+    const target = event.target;
+    const parrent = target.closest('.radio-item');
+    radioStop.disabled = false; //разблокируем кнопку плей
+    audio.src = target.dataset.radioStantion;
+    audio.play();
+    changeIconPlay();
+    selectItem(parrent);
+
+    const title = parrent.querySelector('.radio-name').textContent; //меняем зоголовок на выбранную станцию 
+    radioHeaderBig.textContent = title;
+
+    const urlimg = parrent.querySelector('.radio-img').src; //меняем картику на выбранную станцию 
+    radioCoverImg.src = urlimg;
+  });
+  radioStop.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    changeIconPlay();
+  });
+
+};
--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,44 +1,51 @@
 export const videoPlayerInit = () => {
+  const videoPlayer = document.querySelector(".video-player");
+  const videoButtonPlay = document.querySelector(".video-button__play");
+  const videoButtonStop = document.querySelector(".video-button__stop");
+  const videoProgress = document.querySelector(".video-progress");
+  const videoButtonPassed = document.querySelector(".video-time__passed");
+  const videoButtonTotal = document.querySelector(".video-time__total");
+  const videoVolume = document.querySelector(".video-volume");
+  const videoFullscreen = document.querySelector(".video-button__fullscreen ");
+  const videoVolumeIconUp = document.querySelector(".video-icon-up");
+  const videoVolumeIconDown = document.querySelector(".video-icon-down");
 
-  const videoPlayer = document.querySelector('.video-player');
-  const videoButtonPlay = document.querySelector('.video-button__play');
-  const videoButtonStop = document.querySelector('.video-button__stop');
-  const videoProgress = document.querySelector('.video-progress');
-  const videoButtonPassed = document.querySelector('.video-time__passed');
-  const videoButtonTotal = document.querySelector('.video-time__total');
-
-  const toggleIcon = () => { // меняем кнопку плей на паузу 
+  const toggleIcon = () => {
+    // меняем кнопку плей на паузу
     if (videoPlayer.paused) {
-      videoButtonPlay.classList.remove('fa-pause');
-      videoButtonPlay.classList.add('fa-play');
+      videoButtonPlay.classList.remove("fa-pause");
+      videoButtonPlay.classList.add("fa-play");
     } else {
-      videoButtonPlay.classList.add('fa-pause');
-      videoButtonPlay.classList.remove('fa-play');
+      videoButtonPlay.classList.add("fa-pause");
+      videoButtonPlay.classList.remove("fa-play");
     }
   };
-  const togglePlay = () => { //запуск плеера по нажатию на сам плеер
+  const togglePlay = () => {
+    //запуск плеера по нажатию на сам плеер
     if (videoPlayer.paused) {
       videoPlayer.play();
     } else {
       videoPlayer.pause();
     }
   };
-  const stopPlay = () => { //кнопка стоп
+  const stopPlay = () => {
+    //кнопка стоп
     videoPlayer.pause();
     videoPlayer.currentTime = 0; //возвращаем время видио в начльно время
-  }
-  const addZero = n => n < 10 ? '0' + n : n; //добавляем ноль в начало времени
+  };
+  const addZero = (n) => (n < 10 ? "0" + n : n); //добавляем ноль в начало времени
 
-  videoPlayer.addEventListener('click', togglePlay);
-  videoButtonPlay.addEventListener('click', togglePlay);
+  videoPlayer.addEventListener("click", togglePlay);
+  videoButtonPlay.addEventListener("click", togglePlay);
 
   //запуск togglуIcone
-  videoPlayer.addEventListener('play', toggleIcon);
-  videoPlayer.addEventListener('pause', toggleIcon);
+  videoPlayer.addEventListener("play", toggleIcon);
+  videoPlayer.addEventListener("pause", toggleIcon);
 
-  videoButtonStop.addEventListener('click', stopPlay);
+  videoButtonStop.addEventListener("click", stopPlay);
 
-  videoPlayer.addEventListener('timeupdate', () => { //событие когда обновляется время, время меняется
+  videoPlayer.addEventListener("timeupdate", () => {
+    //событие когда обновляется время, время меняется
     const currentTime = videoPlayer.currentTime;
     const duration = videoPlayer.duration;
 
@@ -50,14 +57,37 @@ export const videoPlayerInit = () => {
     let minuteTotal = Math.floor(duration / 60); //округляем до сотых
     let secondTotal = Math.floor(duration % 60);
 
-    videoButtonPassed.textContent = `${addZero(minutePassed)}:${addZero(secondPassed)}`;
-    videoButtonTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondTotal)}`;
+    videoButtonPassed.textContent = `${addZero(minutePassed)}:${addZero(
+      secondPassed
+    )}`;
+    videoButtonTotal.textContent = `${addZero(minuteTotal)}:${addZero(
+      secondTotal
+    )}`;
   });
 
-  videoProgress.addEventListener('change', () => { //реализуем процесс перемотки
+  videoProgress.addEventListener("input", () => {
+    //реализуем процесс перемотки
     const duration = videoPlayer.duration; //получеам сколько прошло видео
     const value = videoProgress.value;
 
     videoPlayer.currentTime = (value * duration) / 100;
   });
+
+  //развертывание видио на весь экран
+  videoFullscreen.addEventListener("click", () => {
+    videoPlayer.requestFullscreen();
+  });
+  videoVolume.addEventListener("input", () => {
+    videoPlayer.volume = videoVolume.value / 100;
+  });
+
+  videoVolumeIconUp.addEventListener("click", () => {
+    videoVolume.value = 100;
+  });
+  videoVolumeIconDown.addEventListener("click", () => {
+    videoVolume.value = 0;
+  });
+
+  videoPlayer.volume = 0.5; //изначально громкость будет посередине
+  videoVolume.value = videoPlayer.volume * 100; // чтобы позиция оставалась исходной
 };
